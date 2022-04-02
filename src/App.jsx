cb7da0515318a66c15a4c34dc4948a1c8e988766
Faisal Ahammad <faisalahammad24@gmail.com>
2022-04-03 03:09:24 +0600

    Added Search option

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Categories from "./components/Categories";
+import Search from "./components/Search";
 import Pages from "./pages/Pages";
 
 function App() {
   return (
     <div className="App">
+      <Search />
       <Categories />
       <Pages />
     </div>
---
+++ src/pages/Searched.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Searched = () => {
+  const params = useParams();
+  const [searchedRecipes, setSearchedRecipes] = useState([]);
+
+  const getSearched = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${
+        import.meta.env.VITE_API_KEY
+      }&query=${name}`
+    );
+    const recipes = await data.json();
+    setSearchedRecipes(recipes.results);
+  };
+
+  useEffect(() => {
+    getSearched(params.search);
+  }, [params.search]);
+
+  return (
+    <>
+      <h3>Searched for "{params.search}"</h3>
+      <Grid>
+        {searchedRecipes.map((item) => {
+          return (
+            <Card key={item.id}>
+              <img
+                src={
+                  item.image
+                    ? item.image
+                    : "https://via.placeholder.com/556x370.png"
+                }
+                alt={item.title}
+              />
+              <h4>{item.title}</h4>
+            </Card>
+          );
+        })}
+      </Grid>
+    </>
+  );
+};
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
+
+export default Searched;
