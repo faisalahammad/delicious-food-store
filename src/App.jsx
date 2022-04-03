aae8992eacc75063cf3613e44592f50fe17cc5e3
Faisal Ahammad <faisalahammad24@gmail.com>
2022-04-03 21:20:38 +0600

    Added Global Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { GiKnifeFork } from "react-icons/gi";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 import "./App.css";
 import Categories from "./components/Categories";
 import Search from "./components/Search";
@@ -7,11 +10,33 @@ import Pages from "./pages/Pages";
 function App() {
   return (
     <div className="App">
+      <Nav>
+        <GiKnifeFork />
+        <Logo to="/">delicious</Logo>
+      </Nav>
       <Search />
       <Categories />
       <Pages />
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster Two", cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    font-size: 2rem;
+  }
+`;
 
 export default App;
